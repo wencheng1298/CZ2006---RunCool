9a71ae35f6f42d8bfb223387fd284e5ab4d68ead
matthewtnx <71654298+matthewtnx@users.noreply.github.com>
2021-03-09 02:15:38 +0800

    Main menu scroll + top pop up bar

--- runcool/lib/pages/HomePageUI.dart
+++ runcool/lib/pages/HomePageUI.dart
@@ -22,21 +22,68 @@ class HomePageUIState extends State<HomePageUI> {
           style: TextStyle(color: Colors.white),
           textAlign: TextAlign.center,
         ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                print("Menu clicked"); //should change to make it pop up
+              },
+              color: Colors.white54),
+        ],
       ),
-      body: Container(
-        color: Colors.black,
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Here is the home page',
-              style: TextStyle(
-                color: Colors.white,
+      body: SingleChildScrollView( //let the app be scrollable
+        child: Container(
+          color: Colors.black,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 10, bottom: 20, left: 10),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'EVENTS FOR YOU',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
               ),
-            )
-          ],
+              Container(
+                color: Colors.black,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 300,
+                  child: const Card(
+                    child: Text('Event for you card'),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 10, bottom: 20, left: 10),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'See what your friends are up to',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.black,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 300,
+                  child: const Card(
+                    child: Text('Friends up to card'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
