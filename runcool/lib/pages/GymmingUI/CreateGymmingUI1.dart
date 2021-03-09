afcd2f40e788b27e8a77ad0885418d1be002c696
Keith Siow <keith97@live.com>
2021-03-09 20:11:59 +0800

    Added Gymming and Zumba UI

--- runcool/lib/pages/GymmingUI/CreateGymmingUI1.dart
+++ runcool/lib/pages/GymmingUI/CreateGymmingUI1.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:runcool/widgets/GoogleMapPlacement.dart';
+import './../../widgets/InputTextField.dart';
+import './../../widgets/InputFieldTextTitles.dart';
+
+import './../RunningUI/CreateRunningUI2.dart'; // find out if it is possible to use the same page, but change a bit/add on
 
 class CreateGymmingUI1 extends StatefulWidget {
   @override
@@ -6,11 +11,82 @@ class CreateGymmingUI1 extends StatefulWidget {
 }
 
 class _CreateGymmingUI1State extends State<CreateGymmingUI1> {
+  Color _background = Color(0xff1f1b24);
+  Color _turqoise = Color(0xff58C5CC); //?
+
+  void goNextPage() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => CreateRunningUI2())); //find out if need to individually create or can use and add on..
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Gymming UI1"),
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text(
+          'New Routine',
+          style: TextStyle(color: Colors.white),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Container(
+        color: _background,
+        child: Column(
+          children: [
+            GoogleMapPlacement(),
+            Container(
+              height: 410,
+              child: ListView(
+                padding: EdgeInsets.all(8),
+                children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: InputFieldTextTitles('Select a Gym'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8, right: 8),
+                  child: InputTextField(),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: InputFieldTextTitles('Add Workout Routine'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8),
+                  child: Container(
+                    height: 50,
+                    color: _turqoise,
+                    child: const Center(child: Text('Add Workout Exercise')),
+                  )
+                ),
+                ],
+              )
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 20, right: 20),
+                    alignment: Alignment.bottomRight,
+                    child: OutlinedButton(
+                      child: Text('Next'),
+                      onPressed: () => goNextPage(),
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          primary: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- runcool/lib/pages/ZumbaUI/CreateZumbaUI1.dart
+++ runcool/lib/pages/ZumbaUI/CreateZumbaUI1.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:runcool/widgets/GoogleMapPlacement.dart';
+import './../../widgets/InputTextField.dart';
+import './../../widgets/InputFieldTextTitles.dart';
+
+import './../RunningUI/CreateRunningUI2.dart'; // find out if it is possible to use the same page, but change a bit/add on
+
 
 class CreateZumbaUI1 extends StatefulWidget {
   @override
@@ -6,11 +12,90 @@ class CreateZumbaUI1 extends StatefulWidget {
 }
 
 class _CreateZumbaUI1State extends State<CreateZumbaUI1> {
+  Color _background = Color(0xff1f1b24);
+  Color _turqoise = Color(0xff58C5CC); //?
+
+  void goNextPage() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => CreateRunningUI2())); //find out if need to individually create or can use and add on..
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Zumba UI1"),
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text(
+          'New Routine',
+          style: TextStyle(color: Colors.white),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Container(
+        color: _background,
+        child: Column(
+          children: [
+            GoogleMapPlacement(),
+            Container(
+                height: 410,
+                child: ListView(
+                  padding: EdgeInsets.all(8),
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: InputFieldTextTitles('Select a Sports Hall'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8, right: 8),
+                      child: InputTextField(),
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: InputFieldTextTitles('Indicate the Dance Genre'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8, right: 8),
+                      child: InputTextField(),
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: InputFieldTextTitles('Add Dance Music'),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8),
+                        child: Container(
+                          height: 50,
+                          color: _turqoise,
+                          child: const Center(child: Text('Add Dance Music')),
+                        )
+                    ),
+                  ],
+                )
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 20, right: 20),
+                    alignment: Alignment.bottomRight,
+                    child: OutlinedButton(
+                      child: Text('Next'),
+                      onPressed: () => goNextPage(),
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          primary: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
