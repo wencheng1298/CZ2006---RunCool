e209901e223067aec0b5f294ff58b59902c7eea9
noahseah <seahnoah@gmail.com>
2021-03-15 20:43:57 +0800

    Added settings UI

--- runcool/lib/pages/SignUpUI/LogIn.dart
+++ runcool/lib/pages/SignUpUI/LogIn.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:runcool/utils/everythingUtils.dart';
 import 'SignUpUI1.dart';
+import 'package:runcool/pages/SettingUI/SettingsUI.dart';
 
 const textStyle = TextStyle(color: Colors.white, fontSize: 15);
 
