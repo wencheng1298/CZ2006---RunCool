5f956ee9c048c1d2f33173e09667fbba98301268
Keith Siow <keith97@live.com>
2021-03-21 22:32:28 +0800

    initial edit to include Google Maps API

--- runcool/ios/Runner/AppDelegate.swift
+++ runcool/ios/Runner/AppDelegate.swift
@@ -8,6 +8,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // Added in Google API Key
+    GMSServices.provideAPIKey("AIzaSyBIIdfPyfc9sVM5L9Z4_91c5I_k8XXQIG4")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
